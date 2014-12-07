c9403b6981a5b1d06877d82b3539a300502aef99
Eric <suericgp@gmail.com>
2014-12-07 12:48:42 -0600

    Clase 5 finalizada

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	console.log('main.js loaded');
+
+	window.ponyExpress = new PonyExpress({
+		io : window.location.origin
+	});
+
+	window.ponyExpress.bind('connect', function() {
+		window.plugs.article = new PonyExpress.BackbonePlug({
+			collection : window.collections.articles
+		});
+	});
+
+	window.views.app = new Puls3.Views.App( $('body') );
+
+	window.collections.articles = new Puls3.Collections.Articles();
+	
+	window.collections.articles.on('add', function(model) {
+		// Agregar nuevas vistas de articulos aqui
+		var view = new Puls3.Views.Articles({model: model});
+
+		view.render();
+
+		view.$el.prependTo('.posts');
+	});
+
+	window.collections.articles.fetch();	
 });
